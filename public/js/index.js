5d47c058e8146f2ba1fd96b15ad7fd002c2a6131
Marco <stu.marco@gmail.com>
2018-02-15 15:09:49 +0100

    Improve index navigation

--- public/js/index.js
+++ public/js/index.js
@@ -14,4 +14,30 @@ document.addEventListener('DOMContentLoaded', function () {
             });
         });
     });
+
+    const entries = $('.index .row');
+    var currentlyActive = -1;
+    addEvent(document, 'keydown', function (e) {
+        console.log(e.keyCode);
+        if (e.keyCode >= 37 && e.keyCode <= 40) {
+            if (currentlyActive === -1) {
+                currentlyActive = 1;
+            } else if (e.keyCode === 39 || e.keyCode === 40) { //right/down arrow
+                if (++currentlyActive >= entries.length) {
+                    currentlyActive = 1;
+                }
+            } else if (e.keyCode === 37 || e.keyCode === 38) { //left/up arrow
+                if (--currentlyActive <= 0) {
+                    currentlyActive = entries.length - 1;
+                }
+            }
+            entries.forEach(function (row) {
+                row.classList.remove('active');
+            });
+            entries[currentlyActive].classList.add('active');
+        }
+        if(e.keyCode === 13) { //enter
+            location.href = $1('.row.active div a').href;
+        }
+    });
 });
