105885a4f8cec805412f93b073a01d9fd2a6414a
Marco <stu.marco@gmail.com>
2018-06-08 16:31:27 +0200

    Fix bug where up arrow did not automatically select last element on index screen when pressed first

--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,17 @@
 function slideDown(elem) {
     elem.style.maxHeight = '1000px';
     // We're using a timer to set opacity = 0 because setting max-height = 0 doesn't (completely) hide the element.
-    elem.style.opacity   = '1';
+    elem.style.opacity = '1';
 }
 
 function slideUp(elem) {
     elem.style.maxHeight = '0';
-    once( 1, function () {
+    once(1, function () {
         elem.style.opacity = '0';
     });
 }
 
-function once (seconds, callback) {
+function once(seconds, callback) {
     let counter = 0;
     const time = window.setInterval(function () {
         counter++;
@@ -46,10 +46,8 @@ document.addEventListener('DOMContentLoaded', function () {
     let currentlyActive = -1;
     addEvent(document, 'keydown', function (e) {
         if (e.keyCode >= 37 && e.keyCode <= 40) {
-            if (currentlyActive === -1) {
-                currentlyActive = 1;
-            } else if (e.keyCode === 39 || e.keyCode === 40) { //right/down arrow
-                if (++currentlyActive >= entries.length) {
+            if (e.keyCode === 39 || e.keyCode === 40) { //right/down arrow
+                if (++currentlyActive >= entries.length || currentlyActive <= 0) {
                     currentlyActive = 1;
                 }
             } else if (e.keyCode === 37 || e.keyCode === 38) { //left/up arrow
@@ -66,12 +64,12 @@ document.addEventListener('DOMContentLoaded', function () {
         if (e.keyCode === 13) { //enter
             location.href = $1('.row.active div a').href;
         }
-        if(e.keyCode === 69 && search !== document.activeElement) { //E
+        if (e.keyCode === 69 && search !== document.activeElement) { //E
             location.href = $1('.row.active div.option-wrapper a.edit').href;
         }
     });
-    addEvent(tagsHeader, 'click', function() {
-        if(tagsContainer.classList.contains('opened')) {
+    addEvent(tagsHeader, 'click', function () {
+        if (tagsContainer.classList.contains('opened')) {
             tagsContainer.classList.remove('opened');
             tagsHeader.classList.remove('opened');
             slideUp(tagsContainer);
